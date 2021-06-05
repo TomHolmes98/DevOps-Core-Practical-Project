77897dc46b635bed7e55a70868ead4e0c5324f79
Thomas Holmes <82601909+TomHolmes98@users.noreply.github.com>
2021-06-05 15:50:29 +0100

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -6,6 +6,7 @@ sudo apt-get install python3 python3-venv python3-pip -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+pip install wheel
 
 for service in service-2 service-3 service-4 server
 do
