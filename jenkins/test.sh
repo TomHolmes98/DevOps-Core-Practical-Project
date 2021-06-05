4c0c627c295589e8f3f93fda8de512e5126b08e4
Thomas Holmes <82601909+TomHolmes98@users.noreply.github.com>
2021-06-05 15:26:21 +0100

    Update test.sh

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -7,7 +7,7 @@ sudo apt-get install python3 python3-venv python3-pip -y
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r server/requirements.txt
+pip3 install -r requirements.txt
 
 for service in server service-2 service-3 service-4
 do
