3428d9a9049b4fb1cf6df0b652e6c4e0a02390b5
Thomas Holmes <82601909+TomHolmes98@users.noreply.github.com>
2021-06-07 00:04:57 +0100

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+scp docker-compose.yaml docker manager:
 ssh docker-manager << EOF
+
 export DATABASE_URI=${DATABASE_URI}
 
 docker stack deploy --compose-file docker-compose.yaml project
